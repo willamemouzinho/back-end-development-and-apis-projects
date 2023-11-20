ddb35ff7e9178fbd029c2fa8250390231c8a4953
Willame Mouzinho <willame.dev@gmail.com>
2023-11-20 13:37:37 -0300

    ../public path

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static("../public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
